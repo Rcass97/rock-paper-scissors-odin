668a79f54439b64ea5d8d3201d306851af98459b
Reed Cass <michaelreedcass@gmail.com>
2024-09-11 14:18:54 -0600

    Wrote a function to play one round of RPS. Comapres responses and declares a winner. Removed score on index.html

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
-let choiceArray = ["rock", "paper", "scissors"];
+const choiceArray = ["rock", "paper", "scissors"];
 
-function getComputerChoice(){
-    (choiceArray[(Math.floor(Math.random() * choiceArray.length))]);
+function getComputerChoice() {
+    var computerChoice = (choiceArray[(Math.floor(Math.random() * choiceArray.length))]);
+    return computerChoice;
 }
 
-function getHumanChoice(){
-    var userChoice = prompt("Please choose Rock, Paper, or Scissors:");
+function getHumanChoice() {
 
-    if (choiceArray.includes(userChoice.toLowerCase())){
-        alert("You have selected: " + userChoice)
-    }else{
-        alert("You have not entered a valid choice")
+    const humanChoice = prompt("Please choose Rock, Papaer, or Scissors:").toLowerCase()
+
+    if (choiceArray.includes(humanChoice)) {
+        alert("You have selected: " + humanChoice)
+    } else {
+        alert("You have not entered a valid choice. Please try again.")
         getHumanChoice();
+    }
+
+    return humanChoice;
+}
+
+var humanSelection = getHumanChoice();
+var computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice){
+    console.log(humanChoice);
+    console.log(computerChoice);
+
+    if(humanChoice == "rock" && computerChoice == "rock"){
+        alert("You chose: " + humanChoice + "\n" + "The computer chose: " + computerChoice + "\n" + "You have tied!");
+    }else if(humanChoice == "rock" && computerChoice == "paper"){
+        alert("You chose: " + humanChoice + "\n" + "The computer chose: " + computerChoice + "\n" + "You have lost!");
+    }else if(humanChoice == "rock" && computerChoice == "scissors"){
+        alert("You chose: " + humanChoice + "\n" + "The computer chose: " + computerChoice + "\n" + "You have won!");
+    }else if(humanChoice == "paper" && computerChoice == "paper"){
+        alert("You chose: " + humanChoice + "\n" + "The computer chose: " + computerChoice + "\n" + "You have tied!");
+    }else if(humanChoice == "paper" && computerChoice == "scissors"){
+        alert("You chose: " + humanChoice + "\n" + "The computer chose: " + computerChoice + "\n" + "You have lost!");
+    }else if(humanChoice == "paper" && computerChoice == "rock"){
+        alert("You chose: " + humanChoice + "\n" + "The computer chose: " + computerChoice + "\n" + "You have won!");
+    }else if(humanChoice == "scissors" && computerChoice == "scissors"){
+        alert("You chose: " + humanChoice + "\n" + "The computer chose: " + computerChoice + "\n" + "You have tied!");
+    }else if(humanChoice == "scissors" && computerChoice == "rock"){
+        alert("You chose: " + humanChoice + "\n" + "The computer chose: " + computerChoice + "\n" + "You have lost!");
+    }else if(humanChoice == "scissors" && computerChoice == "paper"){
+        alert("You chose: " + humanChoice + "\n" + "The computer chose: " + computerChoice + "\n" + "You have won!");
+    }else{
+        alert("Not working. ERROR.")
     }
 }
 
+playRound(humanSelection, computerSelection);
+
 var humanScore = 0;
 var computerScore = 0;
-
-function playRound(humanChoice, computerChoice){
-    
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
